d7908b1fcc80da9b5da7b10ee3d3d6dc8628541d
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-03-18 05:20:01 +0800

    create retrofit builder

---
+++ app/src/main/java/com/komangss/datasource/network/instance/RetrofitBuilder.kt
@@ -0,0 +1,13 @@
+package com.komangss.datasource.network.instance
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+    private const val BASE_URL = ""
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
