b145295cce650f2254a85d8f648190bf23647b29
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-03-18 05:36:53 +0800

    test shared preference

--- app/src/main/java/com/komangss/ui/MainActivity.kt
+++ app/src/main/java/com/komangss/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.komangss.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import com.komangss.R
 import com.komangss.util.Resource
 
@@ -9,10 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        when(asyap()){
-            is Resource.Error -> TODO()
-            is Resource.Success -> TODO()
-            Resource.InProgress -> TODO()
+//        when(asyap()){
+//            is Resource.Error -> TODO()
+//            is Resource.Success -> TODO()
+//            Resource.InProgress -> TODO()
+//        }
+
+        val pref = getSharedPreferences("p", MODE_PRIVATE)
+
+        pref.edit {
+            putString("key", "ini key")
         }
     }
 
