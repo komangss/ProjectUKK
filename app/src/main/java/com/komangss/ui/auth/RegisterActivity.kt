993b7ac929dddcbe438b086b02a7d229863690a6
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-03-18 22:02:23 +0800

    convert register activity to kotlin

--- app/src/main/java/com/komangss/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/komangss/ui/auth/RegisterActivity.kt
@@ -1,16 +1,12 @@
-package com.komangss.ui.auth;
+package com.komangss.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.komangss.R
 
-import android.os.Bundle;
-
-import com.komangss.R;
-
-public class RegisterActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
     }
 }
