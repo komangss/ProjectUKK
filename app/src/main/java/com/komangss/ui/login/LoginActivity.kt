03fa6577accba8627eb5a9916ca4c2ec1d1b1744
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-03-18 20:24:32 +0800

    delete unused login ViewModel

--- app/src/main/java/com/komangss/ui/login/LoginActivity.kt
+++ app/src/main/java/com/komangss/ui/login/LoginActivity.kt
@@ -2,12 +2,9 @@ package com.komangss.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.komangss.R
 import com.komangss.datasource.network.instance.RetrofitBuilder
-import com.komangss.factory.AuthViewModelFactory
 import com.komangss.repository.AuthRepository
-import com.komangss.ui.login.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
@@ -15,9 +12,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val repo = AuthRepository.getInstance(RetrofitBuilder.authServices)
-        val vmFactory = AuthViewModelFactory.getInstance(repo)
-        val authViewModel = ViewModelProvider(this, vmFactory)[LoginViewModel::class.java]
-        
         btn_activity_main_sign_in.setOnClickListener {
 
         }
--- app/src/main/java/com/komangss/ui/login/viewmodel/LoginViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.komangss.ui.login.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.komangss.repository.AuthRepository
-
-class LoginViewModel(val authRepository: AuthRepository) : ViewModel() {
-
-}
