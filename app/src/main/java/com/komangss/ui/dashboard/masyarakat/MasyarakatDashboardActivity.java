55cf14ebcbdeffdaea48a0eeb33427ec036de37c
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-03-22 04:28:19 +0800

    create activity for masyarakat

---
+++ app/src/main/java/com/komangss/ui/dashboard/masyarakat/MasyarakatDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.komangss.ui.dashboard.masyarakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.komangss.R;
+
+public class MasyarakatDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masyarakat_dashboard);
+    }
+}
